a488be73366e28435238a3b7c31f8fa5b3a2e030
JeffreyYAJ <jeffreyyaj@gmail.com>
2026-05-12 21:03:26 +0100

    Added server script gradio space

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+---
+title: Container Fullness V0
+emoji: 🚀
+colorFrom: green
+colorTo: yellow
+sdk: gradio # Options: gradio, streamlit, docker, or static
+app_file: app.py
+pinned: false
+---
+
 # Container-fullness-dataset
---
+++ app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+from ultralytics import YOLO
+
+# On charge votre fichier .pt
+model = YOLO("yolov8n-cls.pt") 
+
+def predict(image):
+    results = model(image)
+    # On récupère l'image avec les prédictions dessinées dessus
+    return results[0].plot()
+
+# Création de l'interface
+demo = gr.Interface(fn=predict, inputs="image", outputs="image")
+demo.launch()
